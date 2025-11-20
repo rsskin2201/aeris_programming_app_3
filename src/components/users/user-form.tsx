6c2cad298fc5af4070b4e6bf87dd3f2dcc9b4236
Jorge Ricardo Gonzalez Garcia (El Rich) <anto.ganzaga@gmail.com>
2025-11-20 22:41:40 +0000

    Try fixing this error: `Runtime Error: Error: useState is not defined. E

--- src/components/users/user-form.tsx
+++ src/components/users/user-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
