e7a7a4e5295badb2080eddabbaac048d008e55ef
Jorge Ricardo Gonzalez Garcia (El Rich) <anto.ganzaga@gmail.com>
2025-11-28 12:22:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2874147398-14137",
+  "appId": "1:603471231506:web:d0646a814e2f279c1336cb",
+  "apiKey": "AIzaSyBEgDZCJx6xuqHzIhxDIcqCQgWZBXziio8",
+  "authDomain": "studio-2874147398-14137.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "603471231506"
+};
