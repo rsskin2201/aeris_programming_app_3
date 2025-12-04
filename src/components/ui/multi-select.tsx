8ba56a9df72dee8eecfd9bda6643dd5c81332445
Jorge Ricardo Gonzalez Garcia (El Rich) <anto.ganzaga@gmail.com>
2025-12-04 09:46:22 +0000

    El color que utilizaste en el dropdown para la seleccion multiple impide

--- src/components/ui/multi-select.tsx
+++ src/components/ui/multi-select.tsx
@@ -27,9 +27,9 @@ const multiSelectVariants = cva(
     variants: {
       variant: {
         default:
-          "border-foreground/10 text-foreground bg-card hover:bg-card/80",
+          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
         secondary:
-          "border-foreground/10 bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         inverted: "inverted",
